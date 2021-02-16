a0b2b18a4247813c1525161751d9b37d28b0fa71
Gal Bashan <gal@epsagon.com>
2021-02-16 15:40:25 +0200

    [main] Local app working

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const got = require('got');
+const WEATHER_API_URL = 'https://node-congress.workshop.epsagon.com/weather';
+const NEWS_API_URL = 'https://node-congress.workshop.epsagon.com/news';
+const FACT_API_URL = 'https://node-congress.workshop.epsagon.com/fact';
+
+
+function getWeather(city = '') {
+  const URL = `${WEATHER_API_URL}/${city}`
+    return got(URL).json().catch((err) => null); // json
+}
+function getNews(city = '') {
+    const URL = `${NEWS_API_URL}/${city}`
+    return got(URL).json().catch((err) => null); // json
+}
+function getFactForToday() {
+    const d = new Date();
+    const month = d.getMonth() + 1;
+    const day = d.getDate();
+    const URL = `${FACT_API_URL}/${month}/${day}`;
+  return got(URL).text().catch((err) => null); // string
+}
+
+
+const app = express();
+app.get('/digest/:city', async (req, res) => {
+    const city = req.params.city;
+    const [weather, news, fact] = await Promise.all([
+            getWeather(city),
+            getNews(city),
+            getFactForToday()
+        ]);
+    res.json({ weather, news, fact });
+});
+app.use('*', (req, res) => {
+    res.status(404).send('Not Found');
+})
+app.listen(3000, () => console.log('App is now online at port 3000'));
+
+
+
